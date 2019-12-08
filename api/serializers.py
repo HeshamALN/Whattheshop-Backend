c963d3a6a47d452adaff2a0b8bbeaf5e10c0bc01
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-12-08 17:57:29 +0300

    more stuff

--- api/serializers.py
+++ api/serializers.py
@@ -7,14 +7,15 @@ class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name']
+        fields = ['username', 'password', 'first_name', 'last_name', 'email']
 
     def create(self, validated_data):
         username = validated_data['username']
         password = validated_data['password']
         first_name = validated_data['first_name']
         last_name = validated_data['last_name']
-        new_user = User(username=username, first_name=first_name, last_name=last_name)
+        email = validated_data['email']
+        new_user = User(username=username, first_name=first_name, last_name=last_name, email=email)
         new_user.set_password(password)
         new_user.save()
         return validated_data
@@ -68,7 +69,7 @@ class AddToCartSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name', 'email', 'username']
 
     # def get_history(self, obj):
     #     order = Cart.objects.filter(user=obj.user)
